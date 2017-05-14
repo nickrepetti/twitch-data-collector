c3f7b1f424099e224d975ad44ae7d8968ee9aa59
Nick Repetti <nickrepetti@gmail.com>
2017-05-13 21:22:18 -0400

    Updated database schema to include language and game platform

---
+++ game-inserts.sql
@@ -0,0 +1,122 @@
+insert into games(name) values('League of Legends');
+
+insert into games(name) values('Counter-Strike: Global Offensive');
+
+insert into games(name) values('PLAYERUNKNOWN''S BATTLEGROUNDS');
+
+insert into games(name) values('Hearthstone');
+
+insert into games(name) values('Dota 2');
+
+insert into games(name) values('Heroes of the Storm');
+
+insert into games(name) values('Overwatch');
+
+insert into games(name) values('Grand Theft Auto V');
+
+insert into games(name) values('Destiny');
+
+insert into games(name) values('Quake Champions');
+
+insert into games(name) values('The Surge');
+
+insert into games(name) values('Magic: The Gathering');
+
+insert into games(name) values('Street Fighter V');
+
+insert into games(name) values('Tom Clancy''s Rainbow Six: Siege');
+
+insert into games(name) values('World of Warcraft');
+
+insert into games(name) values('IRL');
+
+insert into games(name) values('FIFA 17');
+
+insert into games(name) values('Prey');
+
+insert into games(name) values('H1Z1: King of the Kill');
+
+insert into games(name) values('Creative');
+
+insert into games(name) values('NBA 2K17');
+
+insert into games(name) values('Madden NFL 17');
+
+insert into games(name) values('Minecraft');
+
+insert into games(name) values('RuneScape');
+
+insert into games(name) values('Mario Kart 8');
+
+insert into games(name) values('Dark Souls III');
+
+insert into games(name) values('StarCraft II');
+
+insert into games(name) values('Smite');
+
+insert into games(name) values('Path of Exile');
+
+insert into games(name) values('Halo 5: Guardians');
+
+insert into games(name) values('Talk Shows');
+
+insert into games(name) values('Mass Effect: Andromeda');
+
+insert into games(name) values('Call of Duty: Black Ops III');
+
+insert into games(name) values('Rust');
+
+insert into games(name) values('For Honor');
+
+insert into games(name) values('Gwent: The Witcher Card Game');
+
+insert into games(name) values('Rocket League');
+
+insert into games(name) values('Black Desert Online');
+
+insert into games(name) values('The Legend of Zelda: Breath of the Wild');
+
+insert into games(name) values('Call of Duty: Infinite Warfare');
+
+insert into games(name) values('World of Tanks');
+
+insert into games(name) values('Outlast 2');
+
+insert into games(name) values('Pokémon Sun/Moon');
+
+insert into games(name) values('Diablo III: Reaper of Souls');
+
+insert into games(name) values('Dead by Daylight');
+
+insert into games(name) values('ARK');
+
+insert into games(name) values('Super Smash Bros. for Wii U');
+
+insert into games(name) values('Music');
+
+insert into games(name) values('Final Fantasy XIV: Heavensward');
+
+insert into games(name) values('StarCraft: Brood War');
+
+insert into games(name) values('Super Mario Maker');
+
+insert into games(name) values('Mega Man X3');
+
+insert into games(name) values('Super Smash Bros. Melee');
+
+insert into games(name) values('Paladins');
+
+insert into games(name) values('Kingdom Hearts HD I.5 + II.5 Remix');
+
+insert into games(name) values('The Elder Scrolls V: Skyrim');
+
+insert into games(name) values('Tom Clancy''s The Division');
+
+insert into games(name) values('Call of Duty: Modern Warfare Remastered');
+
+insert into games(name) values('Battlefield 1');
+
+insert into games(name) values('7 Days to Die');
+
+insert into games(name) values('Borderlands 2');
+
--- schema.sql
+++ schema.sql
@@ -1,15 +1,26 @@
-create table Games(
-  GameID             serial2     primary key              not null,
-  Name               text                                 not null
+create table games(
+  gameid             serial2     primary key                      not null,
+  name               text                                         not null
 );
 
-create table GameData(
-  GameDataID         serial4     primary key              not null,
-  GameID             serial2     references Games(GameID) not null,
-  Timestamp          timestamptz default now()            not null,
-  StreamCount        int2                                 not null,
-  ViewerCount        int4                                 not null,
-  PartnerCount       int2                                 not null,
-  PartnerViewerCount int4                                 not null
+create table platforms(
+  platformid         serial2     primary key                      not null,
+  name               text                                         not null
+);
+
+create table gameplatforms(
+  gameid             serial2     references games(gameid)         not null,
+  platformid         serial2     references platforms(platformid) not null
+);
+
+create table gamedata(
+  gamedataid         serial4     primary key                      not null,
+  gameid             serial2     references games(gameid)         not null,
+  timestamp          timestamptz default now()                    not null,
+  language           text                                         not null,
+  streamcount        int2                                         not null,
+  viewercount        int4                                         not null,
+  partnercount       int2                                         not null,
+  partnerviewercount int4                                         not null
 );
 
