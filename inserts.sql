548dbadc5d212d3e1f0cbc83391b1dca507ca288
Nick Repetti <nickrepetti@gmail.com>
2017-05-15 17:57:16 -0400

    Started adding platforms to the game insert statements

--- inserts.sql
+++ inserts.sql
@@ -1,68 +1,126 @@
 -----------------------------------
+------------ Platforms ------------
+-----------------------------------
+insert into platforms(name) values('PC');
+
+insert into platforms(name) values('Xbox');
+
+insert into platforms(name) values('Playstation');
+
+insert into platforms(name) values('Nintendo');
+
+insert into platforms(name) values('Mobile');
+
+-----------------------------------
 ------ Games & GamePlatforms ------
 -----------------------------------
 insert into games(name) values('League of Legends');
 insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'League of Legends' and p.name = 'PC';
 
 insert into games(name) values('Counter-Strike: Global Offensive');
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Counter-Strike: Global Offensive' and p.name = 'PC';
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Counter-Strike: Global Offensive' and p.name = 'Xbox';
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Counter-Strike: Global Offensive' and p.name = 'Playstation';
 
 insert into games(name) values('PLAYERUNKNOWN''S BATTLEGROUNDS');
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'PLAYERUNKNOWN''S BATTLEGROUNDS' and p.name = 'PC';
 
 insert into games(name) values('Hearthstone');
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Hearthstone' and p.name = 'PC';
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Hearthstone' and p.name = 'Mobile';
 
 insert into games(name) values('Dota 2');
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Dota 2' and p.name = 'PC';
 
 insert into games(name) values('Heroes of the Storm');
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Heroes of the Storm' and p.name = 'PC';
 
 insert into games(name) values('Overwatch');
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Overwatch' and p.name = 'PC';
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Overwatch' and p.name = 'Xbox';
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Overwatch' and p.name = 'Playstation';
 
 insert into games(name) values('Grand Theft Auto V');
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Grand Theft Auto V' and p.name = 'PC';
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Grand Theft Auto V' and p.name = 'Xbox';
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Grand Theft Auto V' and p.name = 'Playstation';
 
 insert into games(name) values('Destiny');
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Destiny' and p.name = 'Xbox';
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Destiny' and p.name = 'Playstation';
 
 insert into games(name) values('Quake Champions');
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Quake Champions' and p.name = 'PC';
 
 insert into games(name) values('The Surge');
-
-insert into games(name) values('Magic: The Gathering');
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'The Surge' and p.name = 'PC';
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'The Surge' and p.name = 'Xbox';
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'The Surge' and p.name = 'Playstation';
 
 insert into games(name) values('Street Fighter V');
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Street Fighter V' and p.name = 'PC';
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Street Fighter V' and p.name = 'Playstation';
 
 insert into games(name) values('Tom Clancy''s Rainbow Six: Siege');
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Tom Clancy''s Rainbow Six: Siege' and p.name = 'PC';
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Tom Clancy''s Rainbow Six: Siege' and p.name = 'Xbox';
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Tom Clancy''s Rainbow Six: Siege' and p.name = 'Playstation';
 
 insert into games(name) values('World of Warcraft');
-
-insert into games(name) values('IRL');
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'World of Warcraft' and p.name = 'PC';
 
 insert into games(name) values('FIFA 17');
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'FIFA 17' and p.name = 'PC';
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'FIFA 17' and p.name = 'Xbox';
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'FIFA 17' and p.name = 'Playstation';
 
 insert into games(name) values('Prey');
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Prey' and p.name = 'PC';
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Prey' and p.name = 'Xbox';
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Prey' and p.name = 'Playstation';
 
 insert into games(name) values('H1Z1: King of the Kill');
-
-insert into games(name) values('Creative');
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'H1Z1: King of the Kill' and p.name = 'PC';
 
 insert into games(name) values('NBA 2K17');
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'NBA 2K17' and p.name = 'PC';
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'NBA 2K17' and p.name = 'Xbox';
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'NBA 2K17' and p.name = 'Playstation';
 
 insert into games(name) values('Madden NFL 17');
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Madden NFL 17' and p.name = 'Xbox';
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Madden NFL 17' and p.name = 'Playstation';
 
 insert into games(name) values('Minecraft');
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Minecraft' and p.name = 'PC';
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Minecraft' and p.name = 'Xbox';
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Minecraft' and p.name = 'Playstation';
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Minecraft' and p.name = 'Mobile';
 
 insert into games(name) values('RuneScape');
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'RuneScape' and p.name = 'PC';
 
 insert into games(name) values('Mario Kart 8');
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Mario Kart 8' and p.name = 'Nintendo';
 
 insert into games(name) values('Dark Souls III');
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Dark Souls III' and p.name = 'PC';
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Dark Souls III' and p.name = 'Xbox';
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Dark Souls III' and p.name = 'Playstation';
 
 insert into games(name) values('StarCraft II');
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'StarCraft II' and p.name = 'PC';
 
 insert into games(name) values('Smite');
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Smite' and p.name = 'PC';
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Smite' and p.name = 'Xbox';
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Smite' and p.name = 'Playstation';
 
 insert into games(name) values('Path of Exile');
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Path of Exile' and p.name = 'PC';
+insert into gameplatforms(gameid, platformid) select gameid, platformid from games g, platforms p where g.name = 'Path of Exile' and p.name = 'Xbox';
 
 insert into games(name) values('Halo 5: Guardians');
-
-insert into games(name) values('Talk Shows');
 
 insert into games(name) values('Mass Effect: Andromeda');
 
@@ -96,8 +154,6 @@ insert into games(name) values('ARK');
 
 insert into games(name) values('Super Smash Bros. for Wii U');
 
-insert into games(name) values('Music');
-
 insert into games(name) values('Final Fantasy XIV: Heavensward');
 
 insert into games(name) values('StarCraft: Brood War');
@@ -123,15 +179,4 @@ insert into games(name) values('Battlefield 1');
 insert into games(name) values('7 Days to Die');
 
 insert into games(name) values('Borderlands 2');
-
------------------------------------
------------- Platforms ------------
------------------------------------
-insert into platforms(name) values('Xbox');
-
-insert into platforms(name) values('Playstation');
-
-insert into platforms(name) values('PC');
-
-insert into platforms(name) values('Mobile');
 
