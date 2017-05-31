5f4d2fe3c0d12f3af4a544b1124cd602b09fce75
Nick Repetti <nickrepetti@gmail.com>
2017-05-30 21:56:59 -0400

    More work on connecting serverless to aws

--- handler.js
+++ handler.js
@@ -1,16 +1,206 @@
-'use strict';
+"use strict";
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+const request = require("request");
+const pg = require("pg");
 
-  callback(null, response);
+const PG_URL = process.env.PG_URL;
+const TWITCH_CLIENT_ID = process.env.TWITCH_CLIENT_ID;
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+const client = new pg.Client(PG_URL);
+
+// Number of items to get with each request. Max is 100.
+const limit = 100;
+
+const QUERY_GAMEDATA_CREATE = "insert into gamedata(gameid, language, streamcount, viewercount, partnercount, partnerviewercount) values($1, $2, $3, $4, $5, $6)";
+const QUERY_GAMES_GETALL = "select * from games";
+
+// Default request object initialized with common data/headers so each request 
+// doesn't have to explicitly specify them.
+const req = request.defaults({
+  baseUrl: "https://api.twitch.tv/kraken/",
+  headers: {
+    "Accept": "application/vnd.twitchtv.v5+json",
+    "Client-ID": TWITCH_CLIENT_ID
+  },
+  json: true
+});
+
+module.exports.captureCurrentGames = (event, context, callback) => {
+  // Connect to the PostgreSQL database.
+  client.connect((err) => {
+    if (err) {
+      console.error("An error occurred when connecting to PostgreSQL.");
+      return callback(err);
+    }
+
+    console.info("Opened connection to PostgreSQL.");
+
+    // Grab all games.
+    client.query(QUERY_GAMES_GETALL)
+    .then((result) => {
+      const games = result.rows;
+
+      // Initialize the requests for each game.
+      let requests = games.map((game) => {
+        return new Promise((resolve, reject) => {
+          captureGameData(req, game, limit, resolve, reject);
+        });
+      });
+
+      return Promise.all(requests).then((_) => {
+        console.info("All games successfully captured.");
+      }).catch((err) => {
+        console.error("An error occurred when capturing games.");
+        console.error(err);
+      });
+    })
+    .then((_) => {
+      client.on("drain", client.end.bind(client));
+
+      client.on("end", (_) => {
+        console.info("Closed connection to PostgreSQL.");
+        callback();
+      });
+    });
+  });
 };
+
+// Captures all of the data for a given game. An initial request is made to grab
+// the first page of data as well as the number of additional pages. Based on 
+// that, additional requests are made to capture the rest of the data.
+function captureGameData(req, game, limit, resolve, reject) {
+  const initialOffset = 0;
+  const initialUrl = buildUrl(game.name, limit, initialOffset);
+
+  // Initialize data to be used in the aggregation.
+  let initialPageData;
+
+  // Make initial request.
+  makeRequest(req, initialUrl).then((pageData) => {
+    initialPageData = pageData;
+
+    // Get the number of pages to make requests for.
+    const pageCount = Math.floor(pageData.allStreamCount / limit);
+
+    if (pageCount <= 0) {
+      return Promise.resolve([pageData]);
+    }
+
+    let requests = [];
+
+    // Build a request for each page.
+    for (let i = 0; i < pageCount; i++) {
+      // Calculate the offset for the page.
+      const offset = (i + 1) * limit;
+      const url = buildUrl(game.name, limit, offset);
+
+      requests.push(makeRequest(req, url));
+    }
+
+    return Promise.all(requests);
+  }).then((streams) => {
+    // All data has been captured, reduce into a single result.
+    let gameData = streams.reduce((acc, stream) => {
+      // Loop through all languages.
+      calculateGameData(stream, acc);
+
+      return acc; 
+    }, {});
+
+    // Add data from initial request.
+    calculateGameData(initialPageData, gameData);
+
+    gameData.gameId = game.gameid;
+
+    recordData(gameData, resolve, reject);
+  }).catch((err) => {
+    console.error(err);
+    reject(err);
+  });
+};
+
+// Make a request to get a single page of data.
+function makeRequest(req, url) {
+  return new Promise((resolve, reject) => {
+    req(url, (err, res, body) => {
+      if (err || res.statusCode !== 200) {
+        console.error(err);
+        reject(err);
+      } else {
+        let allData = body.streams.reduce((acc, stream) => {
+          let data = acc[stream.channel.language] || createEmptyGameData();
+
+          data.streamCount++;
+          data.viewerCount += stream.viewers;
+
+          if (stream.channel.partner === true) {
+            data.partnerCount++;
+            data.partnerViewerCount += stream.viewers;
+          }
+
+          acc[stream.channel.language] = data;
+
+          return acc;
+        }, {});
+
+        allData.allStreamCount = body._total;
+
+        resolve(allData);
+      }
+    });
+  });
+}
+
+// Write the results into the database.
+function recordData(gameData, resolve, reject) {
+  for (let language in gameData) {
+    if (gameData.hasOwnProperty(language) && language !== "gameId") {
+      const languageData = gameData[language];
+
+      const args = [gameData.gameId, language, languageData.streamCount, languageData.viewerCount, languageData.partnerCount, languageData.partnerViewerCount];
+
+      client.query(QUERY_GAMEDATA_CREATE, args, (err, result) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        }
+      });
+    }
+  }
+
+  resolve();
+}
+
+// Generate the url for the endpoint to request. Based on the Twitch API v5.
+// https://dev.twitch.tv/docs/v5/reference/streams/#get-live-streams
+function buildUrl(game, limit, offset) {
+  const resource = "streams/";
+  return `${resource}?game=${encodeURIComponent(game)}&limit=${limit}&offset=${offset}`;
+}
+
+function calculateGameData(stream, gameData) {
+  for (let language in stream) {
+    if (stream.hasOwnProperty(language) && language !== "allStreamCount") {
+      let languageData = gameData[language] || createEmptyGameData();
+
+      const languageGameData = stream[language];
+
+      languageData.streamCount += languageGameData.streamCount;
+      languageData.viewerCount += languageGameData.viewerCount;
+      languageData.partnerCount += languageGameData.partnerCount;
+      languageData.partnerViewerCount += languageGameData.partnerViewerCount;
+
+      gameData[language] = languageData;
+    }
+  }
+}
+
+function createEmptyGameData() {
+  return {
+    streamCount: 0,
+    viewerCount: 0,
+    partnerCount: 0,
+    partnerViewerCount: 0
+  };
+}
+
